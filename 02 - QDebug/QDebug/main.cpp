48f5d8d465a3b4cd0dff7aa3ab314e4b03d8ca0f
Peter Panyi <52467332+PanyiP@users.noreply.github.com>
2023-07-14 19:22:49 +0200

    QLoggingCategory

--- 02 - QDebug/QDebug/main.cpp
+++ 02 - QDebug/QDebug/main.cpp
@@ -2,6 +2,12 @@
 #include <QDebug>
 #include "logger.h"
 #include "test.h"
+#include <QLoggingCategory>
+
+// Declare custom logging category
+Q_DECLARE_LOGGING_CATEGORY(network)
+// Define custom logging category
+Q_LOGGING_CATEGORY(network, "network")
 
 int main(int argc, char *argv[])
 {
@@ -20,6 +26,12 @@ int main(int argc, char *argv[])
    t.Testing();
 
    qInfo() << "Finished";
+
+   qInfo(network) << "Custom logging category!: " << network().categoryName();
+
+   // Turn off category logging
+   QLoggingCategory::setFilterRules("network.Debug=false");
+   qDebug(network) << "This should not be logged";
 
    return a.exec();
 }
